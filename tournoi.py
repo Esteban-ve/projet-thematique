09116b7c175a2862643d4a74deff2aa17e4d8a68
arthur-seg <arthur.seguette22@gmail.com>
2025-11-07 16:06:28 +0100

    Correction de la fonction créer_apparaiement_ronde et suppression d'une variable mal généré et ajout de TODO

--- tournoi.py
+++ tournoi.py
@@ -6,7 +6,6 @@ MATCH_NUL = 0
 
 class Tournoi():
     def __init__(self, participants: list, match):
-        self.part
         self.participants = participants
         self.historique_rencontres = {}
         self.n_rondes = 6 # défini en dure temporairement
@@ -19,6 +18,7 @@ class Tournoi():
                 
     
     def resultat_match(self, j1, j2):
+        #TODO le système de gestion de l'historique des rencontres sera généré dans la partie apparaiement
         assert j1 not in self.historique_rencontres[j2]
         try:
             self.historique_rencontres[j1].append(j2)
@@ -38,7 +38,7 @@ class Tournoi():
             return J2_GAGNE
 
     def match_avec_egalite(self,j1,j2):
-        diff=a-b 
+        diff=j2.elo - j1.elo
         p=1/(1 + 10**(-diff/400))
         proba_de_victoire=random()
         if abs (p - proba_de_victoire )< 0.1:
@@ -59,18 +59,17 @@ class Tournoi():
                                      key=lambda j: (self.résultats[j.nom], j.elo), 
                                      reverse=True) # on obtiennt directement un classement dans l'ordre décroissant des résultats puis de l'élo initial des joueurs
         apparaiement = []
+        #TODO on ne peut pas valider l'apparaiement sans avoir vérifier qu'il ne respectait pas toutes les contraintes (ex: joueurs déja rencontrés, exempt, etc)
         if n_participants %2 == 0:
             for i in range (n_participants/2):
-                apparaiement
-append((participants_classé))
-                          apparaiement.append((participants_classé,))        self.ronde += 1
+                apparaiement.append((participants_classé[i],participants_classé[i+n/2]))
+        else:
+            #TODO à écrir
+            pass
+
+        return apparaiement
     
 
 
 
 
-
-    def classement_temoin(self):
-        parti
-
-c
