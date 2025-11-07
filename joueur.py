7eacfb780d90df7025a4a79980f538b5013fb87a
arthur-seg <arthur.seguette22@gmail.com>
2025-11-07 15:54:16 +0100

    classe Joueur : K inclu dans le constructeur

--- joueur.py
+++ joueur.py
@@ -1,6 +1,6 @@
 class Joueur():
-    def __init__(self, nom, niveau):
+    def __init__(self, nom, niveau, K=32):
         self.nom = nom
         self._niveau = niveau
         self.elo = niveau  # pour l'instant l'elo reflete parfaitement le niveau intrinsèque
-        self.K = 32  # facteur de correction de l'elo
+        self.K = K  # facteur de correction de l'elo
